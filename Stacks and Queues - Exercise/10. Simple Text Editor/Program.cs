d7508f57e9343e956a44e93692c5329ab3f823bc
morskibg <dimityrp@yahoo.com>
2018-01-23 19:45:23 +0200

    +

--- Stacks and Queues - Exercise/10. Simple Text Editor/Program.cs
+++ Stacks and Queues - Exercise/10. Simple Text Editor/Program.cs
@@ -12,7 +12,7 @@ namespace _10.Simple_Text_Editor
         {
             int n = int.Parse(Console.ReadLine());
             StringBuilder SB = new StringBuilder();
-            Queue<string> queue = new Queue<string>();
+            Stack<string> stack = new Stack<string>();
             for (int i = 0; i < n; ++i)
             {
                 string[] commands = Console.ReadLine().Split(' ').ToArray();
@@ -21,25 +21,17 @@ namespace _10.Simple_Text_Editor
                 {
                     case 1:
                         {
-                            if (queue.Count == 2)
-                            {
-                                queue.Dequeue();
-                            }
-                            queue.Enqueue(commands[1]);
+                            stack.Push("!" + commands[1]);
                             SB.Append(commands[1]);
                             break;
                         }
                     case 2:
                         {
-                            if (queue.Count == 2)
-                            {
-                                queue.Dequeue();
-                            }
-                            
+
                             int eraseCount = int.Parse(commands[1]);
                             char[] toRemoveArr = new char[eraseCount];
                             SB.CopyTo(SB.Length - eraseCount, toRemoveArr, 0, eraseCount);
-                            queue.Enqueue(new string(toRemoveArr));
+                            stack.Push(new string(toRemoveArr));
                             SB.Remove(SB.Length - eraseCount, eraseCount);
                             break;
                         }
@@ -49,22 +41,31 @@ namespace _10.Simple_Text_Editor
                             if (idx >= 0 && idx < SB.Length)
                             {
                                 Console.WriteLine(SB[idx]);
+
                             }
                             break;
                         }
                     case 4:
                         {
-                            if(queue.Count > 0)
-                            {                                
-                                string fromQueue = queue.Dequeue();
-                                SB.Remove(SB.Length - fromQueue.Length, fromQueue.Length);
-                                SB.Append(fromQueue);
-                            }                       
+                            if (stack.Count > 0)
+                            {
+                                string fromStack = stack.Pop();
+                                if (fromStack.Contains("!"))
+                                {
+                                    string toRemove = fromStack.Substring(1, fromStack.Length - 1);
+                                    SB.Remove(SB.Length - toRemove.Length, toRemove.Length);
+                                }
+                                else
+                                {
+
+                                    SB.Append(fromStack);
+                                }
+
+                            }
 
                             break;
                         }
                 }
-
             }
         }
     }
