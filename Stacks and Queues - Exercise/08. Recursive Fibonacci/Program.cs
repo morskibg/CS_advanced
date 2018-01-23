619d572faf3c14cf6dc4b62436c7ffea278cb32c
morskibg <dimityrp@yahoo.com>
2018-01-23 15:56:17 +0200

    +

---
+++ Stacks and Queues - Exercise/08. Recursive Fibonacci/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Recursive_Fibonacci
+{
+    class Program
+    {
+        static long[] lookup = new long[51];
+        static long GetFib(long num)
+        {
+            if(num == 1 || num == 2)
+            {
+                lookup[1] = 1;
+                lookup[2] = 1;
+                return 1;
+            }
+            if(lookup[num - 1] == 0)
+            {
+                long calcFib = GetFib(num - 1) + GetFib(num - 2);
+                lookup[num] = calcFib;
+            }
+            return lookup[num - 1] + lookup[num - 2]; 
+            
+           
+        }
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());  
+            long fib = GetFib(n);
+            Console.WriteLine(fib);
+           
+        }
+    }
+}
