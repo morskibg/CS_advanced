6cc3cd83a0ce2c35c3b4d69a72dc0e27082b50d0
morskibg <dimityrp@yahoo.com>
2018-02-06 23:12:12 +0200

    +

--- OldExams/02. Knight Game/Program.cs
+++ OldExams/02. Knight Game/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,114 @@ namespace _02.Knight_Game
 {
     class Program
     {
+        struct Point
+        {
+            public int Row { get; set; }
+            public int Col { get; set; }
+
+            public Point(int i, int j)
+            {
+                this.Row = i;
+                this.Col= j;
+            }
+
+        }
+        static bool IsInMatrix(int n ,int newRow, int newCol)
+        {
+            return newRow >= 0 && newRow < n && newCol >= 0 && newCol < n;
+        }
+
+        static int HowManyCanAttack(int currRow, int currCol, char[,] matrix, int n)
+        {
+            int victims = 0;
+            if (IsInMatrix(n, currRow - 1, currCol - 2) && matrix[currRow - 1, currCol - 2] == 'K')
+            {
+                ++victims;
+            }
+            if (IsInMatrix(n, currRow - 2, currCol - 1) && matrix[currRow - 2, currCol - 1] == 'K')
+            {
+                ++victims;
+            }
+            if (IsInMatrix(n, currRow - 2, currCol + 1) && matrix[currRow -2 , currCol + 1] == 'K')
+            {
+                ++victims;
+            }
+            if (IsInMatrix(n, currRow - 1, currCol + 2) && matrix[currRow - 1, currCol + 2] == 'K')
+            {
+                ++victims;
+            }
+            if (IsInMatrix(n, currRow + 1, currCol + 2) && matrix[currRow + 1, currCol + 2] == 'K')
+            {
+                ++victims;
+            }
+            if (IsInMatrix(n, currRow + 2, currCol + 1) && matrix[currRow + 2, currCol + 1] == 'K')
+            {
+                ++victims;
+            }
+            if (IsInMatrix(n, currRow + 2, currCol - 1) && matrix[currRow + 2, currCol - 1] == 'K')
+            {
+                ++victims;
+            }
+            if (IsInMatrix(n, currRow + 1, currCol - 2) && matrix[currRow + 1, currCol - 2] == 'K')
+            {
+                ++victims;
+            }
+
+            return victims;
+        }
+
+        static void Print(char[,] matrix, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n,n];
+            List<Point> knights = new List<Point>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                for (int j = 0; j < n; j++)
+                {
+                    if (input[j] == 'K')
+                    {
+                        knights.Add(new Point(i, j));
+                    }
+                    matrix[i, j] = input[j];
+                }
+            }
+            List<int> victims = new List<int>(knights.Count);
+            for (int i = 0; i < knights.Count; i++)
+            {
+                Point currKnight = knights[i];
+                victims.Add(HowManyCanAttack(currKnight.Row, currKnight.Col, matrix, n));
+            }
+            int initialKnights = knights.Count;
+            while (victims.Sum() != 0)
+            {
+
+                int maxViolentKnight = victims.IndexOf(victims.Max());
+                matrix[knights[maxViolentKnight].Col, knights[maxViolentKnight].Row] = '0';
+                knights.RemoveAt(maxViolentKnight);
+                victims.Clear();
+                for (int i = 0; i < knights.Count; i++)
+                {
+                    Point currKnight = knights[i];
+                    victims.Add(HowManyCanAttack(currKnight.Row, currKnight.Col, matrix, n));
+                }
+
+            }
+            Print(matrix, n);
+            Console.WriteLine(initialKnights - knights.Count);
+            int t = 0;
         }
     }
 }
