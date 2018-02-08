6c074284429d22bb42020b4cbddb27e45f5dc677
morskibg <dimityrp@yahoo.com>
2018-02-08 14:12:06 +0200

    +

--- OldExams/04. Hospital/Program.cs
+++ OldExams/04. Hospital/Program.cs
@@ -11,21 +11,21 @@ namespace _04.Hospital
     {
         struct Patient
         {
-            public string FullName { get; set; }
+            public string PatientsName { get; set; }
             public string Department { get; set; }
             public string Doctor { get; set; }
             public int BedNumber { get; set; }
 
-            public Patient(string fullName, string department, string doctor)
+            public Patient(string patientsName, string department, string doctor)
             {
-                this.FullName = fullName;
+                this.PatientsName = patientsName;
                 this.Department = department;
                 this.Doctor = doctor;
                 this.BedNumber = 0;
             }
         }
         static void Main(string[] args)
-        {
+        {         
             List<Patient> database = new List<Patient>();
             while (true)
             {
@@ -36,13 +36,15 @@ namespace _04.Hospital
                 }
                 string[] tokens = input.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
                     .Select(x => x.Trim()).ToArray();
-                string doctorsName = tokens[1] + tokens[2];
-                Patient currPatient = new Patient(tokens[3], tokens[0], doctorsName);
+                string doctorsName = tokens[1] + " " + tokens[2];
+                string patientsName = tokens[3];
+                string department = tokens[0];
+                Patient currPatient = new Patient(patientsName, department, doctorsName);
                 int lastOccupedBedInDepartment = 0;
 
-                if (database.Where(x => x.Department == tokens[0]).Count() > 0)
+                if (database.Where(x => x.Department == department).Count() > 0)
                 {
-                    lastOccupedBedInDepartment = database.Where(x => x.Department == tokens[0])
+                    lastOccupedBedInDepartment = database.Where(x => x.Department == department)
                         .OrderByDescending(x => x.BedNumber).First().BedNumber;
                 }
                 if (lastOccupedBedInDepartment == 60)
@@ -59,27 +61,30 @@ namespace _04.Hospital
                 {
                     break;
                 }
-                string pattern = @"(?<first_name>[A-Za-z]+)\s*(?<second_name>[A-Za-z]*)\s*(?<digit>\d)*";
+                string pattern = @"(?<first_name>\b[^\s]+)\s*(?<digit>\b[\d]*)\s*(?<second_name>\b[^\s]*)";
                 Regex rg = new Regex(pattern);
                 Match mc = rg.Match(input);
-                
-                if (mc.Groups["second_name"].Value != "")
+
+                if (mc.Groups["digit"].Value != "")
                 {
-                    string doctor = mc.Groups["first_name"].Value + mc.Groups["second_name"].Value;
-                    foreach (var patient in database.Where(x => x.Doctor == doctor).OrderBy(x => x.FullName))
+                    int roomToPrintPatients = int.Parse(mc.Groups["digit"].Value);
+
+                    foreach (var patient in database.Where(x => x.Department == mc.Groups["first_name"].Value)
+                        .Where(x => x.BedNumber <= roomToPrintPatients * 3 && x.BedNumber > roomToPrintPatients * 3 - 3)
+                        .OrderBy(x => x.PatientsName))
                     {
-                        Console.WriteLine(patient.FullName);
+                        Console.WriteLine(patient.PatientsName);
                     }
                 }
-                
-                else if (mc.Groups["digit"].Value != "")
+
+                else if (mc.Groups["second_name"].Value != "")
                 {
-                    int roomTiPrintPatient = int.Parse(mc.Groups["digit"].Value);
-                    foreach (var patient in database.Where(x => x.Department == mc.Groups["first_name"].Value)
-                        .Where(x => x.BedNumber <= roomTiPrintPatient * 3 && x.BedNumber > (roomTiPrintPatient - 1) * 3)
-                        .OrderBy(x => x.FullName))
+
+                    string doctor = mc.Groups["first_name"].Value + " " + mc.Groups["second_name"].Value;
+
+                    foreach (var patient in database.Where(x => x.Doctor == doctor).OrderBy(x => x.PatientsName))
                     {
-                        Console.WriteLine(patient.FullName);
+                        Console.WriteLine(patient.PatientsName);
                     }
                 }
                 else
@@ -87,7 +92,7 @@ namespace _04.Hospital
                     foreach (var patient in database.Where(x => x.Department == mc.Groups["first_name"].Value)
                         .OrderBy(x => x.BedNumber))
                     {
-                        Console.WriteLine(patient.FullName);
+                        Console.WriteLine($"{patient.PatientsName}");
                     }
                 }
 
