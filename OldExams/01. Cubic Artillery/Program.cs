4544ad7a45ea6ac22bd6e3dd00005361b9578353
morskibg <dimityrp@yahoo.com>
2018-02-09 13:11:29 +0200

    +

--- OldExams/01. Cubic Artillery/Program.cs
+++ OldExams/01. Cubic Artillery/Program.cs
@@ -21,74 +21,114 @@ namespace _01.Cubic_Artillery
                 else
                 {
                     return remainingBunkerCapacity;
-                }                
+                }
             }
-            return 0;
+            return remainingBunkerCapacity;
         }
-        static void RearangeLastBunker(Queue<int> bunker, int weapon,  int remainingCapacity)
+        static int RearangeLastBunker(Queue<int> bunker, int weapon, int remainingCapacity)
         {
-            Queue<int> tempStorage = new Queue<int>();
+            
+
             while (true)
             {
-                int removedWeapon = bunker.Dequeue();
-                tempStorage.Enqueue(removedWeapon);
+                int removedWeapon = bunker.Dequeue();               
                 remainingCapacity += removedWeapon;
-                if(remainingCapacity >= weapon)
+                if (remainingCapacity >= weapon)
                 {
                     bunker.Enqueue(weapon);
                     break;
                 }
             }
+            return remainingCapacity;
         }
         static void Main(string[] args)
         {
             int bunkerCapacity = int.Parse(Console.ReadLine());
             Queue<string> inputBunkers = new Queue<string>();
             Queue<int> inputWeapons = new Queue<int>();
-            Dictionary<string, Queue<int>> bunkers = new Dictionary<string, Queue<int>>();
+            Dictionary<string, Queue<int>> filledBunkers = new Dictionary<string, Queue<int>>();
+
             int tryParse = 0;
             while (true)
             {
                 string input = Console.ReadLine();
-                if(input == "Bunker Revision")
+                if (input == "Bunker Revision")
                 {
                     break;
                 }
                 string[] tokens = input.Split().ToArray();
                 foreach (var i in tokens)
                 {
-                    if(int.TryParse(i, out tryParse))
+                    if (int.TryParse(i, out tryParse))
                     {
-                        if(bunkerCapacity >= tryParse)
+                        if (bunkerCapacity >= tryParse)
                         {
                             inputWeapons.Enqueue(tryParse);
-                        }                        
+                        }
                     }
                     else
                     {
                         inputBunkers.Enqueue(i);
                     }
                 }
-                
+
                 int currCapacity = bunkerCapacity;
                 if (inputBunkers.Count == 0)
                 {
                     break;
                 }
-                string currBunker = inputBunkers.Dequeue();
-                Queue<int> currBunkerStash = new Queue<int>();
-                currCapacity = AddWeapon(currBunkerStash, inputWeapons, currCapacity);
-                if (currCapacity == 0)
+                while (true)
                 {
-                    Console.WriteLine($"{currBunker} -> {string.Join(", ",currBunkerStash)}");
-                }
-                else
-                {
-                    if(bunkers.Count == 1)
+                    if (inputBunkers.Count > 0 && filledBunkers.Count > 0)
                     {
-                        RearangeLastBunker(currBunkerStash, inputWeapons.Dequeue(), currCapacity);
+                        foreach (var kvp in filledBunkers)
+                        {
+                            if (kvp.Value.Count > 0)
+                            {
+                                Console.WriteLine($"{kvp.Key} -> {string.Join(", ", kvp.Value)}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{kvp.Key} -> Empty");
+                            }
+                        }
+                        filledBunkers.Clear();
                     }
-                    int p = 0;
+
+
+                    string currBunker = inputBunkers.Dequeue();
+                    Queue<int> currBunkerStash = new Queue<int>();
+                    currCapacity = AddWeapon(currBunkerStash, inputWeapons, currCapacity);
+                    if (currCapacity == 0 && inputBunkers.Count > 0)
+                    {
+
+                        Console.WriteLine($"{currBunker} -> {string.Join(", ", currBunkerStash)}");
+                        currCapacity = bunkerCapacity;
+                    }
+                    else if(currCapacity == bunkerCapacity && inputBunkers.Count > 0)
+                    {
+                        Console.WriteLine($"{currBunker} -> Empty");
+                    }
+                    else
+                    {
+                        if (inputBunkers.Count == 0)
+                        {
+                            
+                            while (inputWeapons.Count > 0)
+                            {
+                                currCapacity = RearangeLastBunker(currBunkerStash, inputWeapons.Dequeue(), currCapacity);
+                            }
+                            filledBunkers[currBunker] = new Queue<int>(currBunkerStash);
+
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{currBunker} -> {string.Join(", ", currBunkerStash)}");
+                            break;
+                        }
+
+                    }
                 }
                 int t = 0;
             }
