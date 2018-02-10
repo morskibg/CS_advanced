f0b25c5a974befc490232ea9392c0dbbbf3f2dbb
morskibg <dimityrp@yahoo.com>
2018-02-10 11:12:02 +0200

    +

--- OldExams/02. Monopoly/Program.cs
+++ OldExams/02. Monopoly/Program.cs
@@ -8,8 +8,76 @@ namespace _02.Monopoly
 {
     class Program
     {
+        static bool IsInRange(int row, int col, int currRow, int currCol)
+        {
+            return currRow >= 0 && currRow < row && currCol >= 0 && currCol < col;
+        }
         static void Main(string[] args)
         {
+            int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int row = dimensions[0];
+            int col = dimensions[1];
+            char[,] matrix = new char[row, col];
+            for (int i = 0; i < row; i++)
+            {
+                string input = Console.ReadLine();
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+            int money = 50;
+            int turns = 0;
+            int incomePerTurn = 0;
+            int hotels = 0;
+            int jailTime = 0;
+            //Queue<int> path = new Queue<int>(row * col);
+            for (int i = 0; i < row; i++)
+            {
+                bool goesToTheRight = i % 2 == 0;
+                for (int j = 0, k = col - 1; j < col || jailTime > 0; j++, --k)
+                {
+
+
+                    if (jailTime > 0)
+                    {
+                        --j;
+                        ++k;
+                        --jailTime;
+                        ++turns;
+                        money += 10 * hotels;
+                        continue;
+                    }
+
+                    int idx = goesToTheRight ? j : k;
+                    char currCell = matrix[i, idx];
+                    if (currCell == 'H')
+                    {
+                        Console.WriteLine($"Bought a hotel for {money}. Total hotels: {++hotels}.");
+                        money = 0;
+                    }
+                    else if (currCell == 'J')
+                    {
+                        Console.WriteLine($"Gone to jail at turn {turns}.");
+                        jailTime = 2;
+                    }
+                    else if (currCell == 'S')
+                    {
+                        int moneyToSpent = (idx + 1) * (i + 1);
+                        if (money < moneyToSpent)
+                        {
+                            moneyToSpent = money;
+                        }
+                        Console.WriteLine($"Spent {moneyToSpent} money at the shop.");
+                        money -= moneyToSpent;
+                    }
+                    ++turns;
+                    money += 10 * hotels;
+                }
+            }
+            Console.WriteLine($"Turns {turns}");
+            Console.WriteLine($"Money {money}");
+            int t = 0;
         }
     }
 }
---
+++ OldExams/03. Basic Markup Language/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _03.Basic_Markup_Language
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            //"\s*<\s*([a-z]+)\s+(?:value\s*=\s*\"\s*(\d+)\s*\"\s+)?[a-z]+\s*=\s*\"([^""]*)\"\s*\/>\s*"
+            string pattern =
+                @"(?:<)(?<command1>\s*[a-zA-Z]+\s+[a-zA-Z]+\s*)\s*=\s*\""(?<quot1>[^""]*)\""\s*(?<command2>[a-zA-Z]+)?\s*=?\s*\""?(?<quot2>[^""]*)?\""?\s*(?:\/>$)";
+            Regex rx = new Regex(pattern);
+            int counter = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "<stop/>")
+                {
+                    break;
+                }
+
+                //MatchCollection matches = Regex.Matches(input, pattern);
+                //var list = matches.Cast<Match>().Select(x => x.Value).ToList();
+                //Console.WriteLine("******************************8");
+                //Console.WriteLine(string.Join(", ",list));
+                Match match = rx.Match(input);
+                string firstComamnd = match.Groups["command1"].Value;
+
+                if (firstComamnd.Contains("repeat"))
+                {
+                    int repeatValue = 0;
+                    bool isDigit = int.TryParse(match.Groups["quot1"].Value, out repeatValue);
+                    if (!isDigit)
+                    {
+                        continue;
+                    }
+                    string toRepeat = match.Groups["quot2"].Value;
+                    counter = repeatValue == 0 ? counter : ++counter;
+                    for (int i = 0; i < repeatValue; i++)
+                    {
+                        Console.WriteLine($"{counter}. {toRepeat}");
+                        counter = i == repeatValue - 1 ? counter : ++counter;
+                    }
+                    //Console.WriteLine($"Value to repeat {repeatValue}");
+                }
+                else if (firstComamnd.Contains("reverse"))
+                {
+                    
+                    
+                    string toReverse = match.Groups["quot1"].Value;
+                    if (toReverse.Length == 0)
+                    {
+                        continue;
+                    }
+                    ++counter;
+                    Console.WriteLine($"{counter}. {string.Join("", toReverse.Reverse())}");
+                    //Console.WriteLine($"Cotent to reverse {toReverse}");
+                }
+                else if (firstComamnd.Contains("inverse"))
+                {
+                    
+                    string toInverse = match.Groups["quot1"].Value;
+                    if (toInverse.Length == 0)
+                    {
+                        continue;
+                    }
+                    ++counter;
+                    Console.WriteLine($"{counter}. {string.Join("", toInverse.Select(x => char.IsLower(x) ? char.ToUpper(x) : char.ToLower(x)))}");
+                    //Console.WriteLine($"Cotent to inverse {toInverse}");
+                }
+
+            }
+        }
+    }
+}
